79804d93dbe8fd4ef33eec9f8eb17bdec3f7a629
Anna Tsalapova <anna.tsalapova@compscicenter.ru>
2016-10-09 22:08:15 +0300

    main.cpp changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,52 @@
 #include <iostream>
+#include <list>
 #include "DBLPManager.h"
 #include "ArticleInfo.h"
-#include <list>
 #include "parser.h"
 
 using namespace std;
 
 int main (int argc, char ** argv) {
-
+	list<string> auths;
+	DBLPManager dblp;
+	int result_size = 0;
 	if (argv[1] == "A") {
-
 		parser * pr;
 		string file_name; 
-		if (argc > 1){
-			try{
-			file_name = string(argv[1]);
-			pr = new parser(file_name);
+		if (argc > 2){
+			try {
+				file_name = string(argv[2]);
+				pr = new parser(file_name);
 			} catch(...) {}
-		} else {		
+		} else {
 			cin >> file_name;
 			pr = new parser(file_name);
-	    }
-	    
-	    list<string> auths = pr->get_authors();
-	    for (list<string>::const_iterator it = auths.begin(); it != auths.end(); ++it){
-	    	cout << *it << endl;
-	    }
-	    
-	    delete pr;
-	
+		}
+		auths = pr->get_authors();
+		cout << "------------------parser-----------------" << endl;
+		for (list<string>::const_iterator it = auths.begin(); it != auths.end(); ++it) {
+			cout << *it << endl;
+		}
+		cout << "------------------dblp-----------------" << endl;
+		for (list<string>::const_iterator it = auths.begin(); it != auths.end(); ++it) {
+			vector <ArticleInfo> result = dblp.publicationRequest(*it);
+			result_size = result.size();
+			for (unsigned int k = 0; k < result_size; ++k) {
+				cout << result[k].to_String() << " \n"; 
+			}
+		}
+		delete pr;
 	}
 	else if (argv[1] == "Y"){
-		for (int i = 2; i < argc; i++) {	
-			DBLPManager dblp;
+		for (int i = 2; i < argc; i++) {			
 			vector <ArticleInfo> result = dblp.publicationRequest(string(argv[i]));
-			for (unsigned int k = 0; k < result.size(); k++) {
-				std::cout << result[k].to_String() << " \n"; 
+			result_size = result.size();
+			for (unsigned int k = 0; k < result_size; ++k) {
+				cout << result[k].to_String() << " \n"; 
 			}
-		}	
+		}
 	}
-  	return 0;
-	}
+	return 0;
+}
 
 
