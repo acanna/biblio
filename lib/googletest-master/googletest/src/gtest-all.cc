9503093a55a075140f176dfd657d264b1728ae16
YanaVKuznetsova <kuznetsova.jana@gmail.com>
2016-11-17 00:18:23 +0300

    Gtest fixed

--- lib/googletest-master/googletest/src/gtest-all.cc
+++ lib/googletest-master/googletest/src/gtest-all.cc
@@ -38,3 +38,14 @@
 // when it's fused.
 
 // The following lines pull in the real gtest *.cc files.
+
+#include "gtest/gtest.h"
+
+// The following lines pull in the real gtest *.cc files.
+#include "src/gtest.cc"
+#include "src/gtest-death-test.cc"
+#include "src/gtest-filepath.cc"
+#include "src/gtest-port.cc"
+#include "src/gtest-printers.cc"
+#include "src/gtest-test-part.cc"
+#include "src/gtest-typed-test.cc"
